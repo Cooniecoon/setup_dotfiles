dd2718a623d37fa9eeddc406bcae54e736328024
cooniecoon <hoony3355@gmail.com>
2025-03-15 15:25:49 +0900

    add install docker
    Signed-off-by: cooniecoon <hoony3355@gmail.com>

--- install_commands.sh
+++ install_commands.sh
@@ -1,9 +1,34 @@
 source ./ros2_setup.sh
 
 function install_basic() {
- 	sudo apt install -y git cmake vim
-
+ 	sudo apt install -y git cmake vim openssh-server openssh-client curl wget htop
  }
+
+function install_docker() {
+	# Add Docker's official GPG key:
+	sudo apt-get update
+	sudo apt-get install ca-certificates curl
+	sudo install -m 0755 -d /etc/apt/keyrings
+	sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+	sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+	# Add the repository to Apt sources:
+	echo \
+	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+	$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+	sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	sudo apt-get update
+
+	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+	sudo groupadd docker
+	sudo usermod -aG docker $USER
+	sudo chmod 666 /var/run/docker.sock
+
+	sudo service docker start
+
+
+}
 
  function install_vscode() {
 	sudo apt install software-properties-common apt-transport-https wget -y
